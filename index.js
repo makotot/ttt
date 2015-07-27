c7d2c52e9f9de1ab06d5f31cd4d63898970be598
makotot <makoto.ttn@gmail.com>
2015-07-27 13:49:06 +0900

    + iconv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var got = require('got'),
-  cheerio = require('cheerio');
+  cheerio = require('cheerio'),
+  Iconv = require('iconv').Iconv,
+  chardet = require('jschardet');
+
 
 module.exports = function (url, cb) {
 
@@ -7,12 +10,15 @@ module.exports = function (url, cb) {
     cb(new Error('Url is required.'), null);
   }
 
-  got(url, function (err, data, res) {
+  got(url, {encoding: null}, function (err, data, res) {
     if (err) {
       cb(err, null);
     }
+    var encoding = chardet.detect(data).encoding,
+      iconv = new Iconv(encoding, 'UTF-8//TRANSLIT//IGNORE'),
+      converted = iconv.convert(data);
 
-    var $ = cheerio.load(data);
+    var $ = cheerio.load(converted.toString('utf8'));
 
     cb(null, $('title').text());
   });
